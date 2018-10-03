9c195ccb981b3e8c1b5180d6339e6727335c4643
datamine <contact@johnloeber.com>
2018-10-02 21:24:16 -0700

    contribution guidelines & other malarkey

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Most-Expensive-Tweets
 You can burn a lot of money in 280 chars
+
+## Contribution Guidelines
+
+- Edit `expensive.json`
+- Additions must be correctly sorted in the JSON file (from largest to smallest)
+- Descriptions may be no more than 140 characters
--- index.js
+++ index.js
@@ -3,7 +3,32 @@ $(document).ready(function() {
         url: "https://raw.githubusercontent.com/Datamine/Most-Expensive-Tweets/master/expensive.json",
         dataType: "json",
         success: function(response) {
-            console.log(response);
+            twttr.ready(function (twttr) {
+                for (var i = 0; i < response.length; i++) {
+                    let tweetblob = response[i]
+                    $('#tweet-container').append(`
+                        <div class='center-container subheader'>
+                            #${i+1}
+                        </div>
+                        <div class='center-container' id='${i}'>
+                        </div>
+                    `);
+                    twttr.widgets.createTweet(
+                      tweetblob.status_id,
+                      document.getElementById(`${i}`),
+                    );
+                    $('#tweet-container').append(`
+                        <div class='center-container'>
+                            <p class='description'>
+                                Cost: <b>${tweetblob.cost_usd}</b>
+                            </p>
+                            <p class='description' style='text-align:justify;'>
+                                ${tweetblob.description}
+                            </p>
+                        </div>
+                    `);
+                }
+            });
         }
     });
 });
