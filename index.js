66693b1d176b72036d6de19de13d14f47d7c26dd
datamine <contact@johnloeber.com>
2018-10-02 20:42:13 -0700

    Load JSON via index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://raw.githubusercontent.com/cben/sandbox/master/json/index.json",
-        dataType: "json"
-    }).done(function(result){
-        for (var item in result) {
-            console.log(item);
+        url: "https://raw.githubusercontent.com/Datamine/Most-Expensive-Tweets/master/expensive.json",
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
         }
     });
 });
